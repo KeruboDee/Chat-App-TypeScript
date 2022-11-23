786d3cbda45c3a5962907d20ecebd008684a1a90
Deborah Kerubo <deborah.kerubo@mtnbusiness.co.ke>
2022-11-23 16:21:01 +0300

    first chat app

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+const{ listen } = require("socket.io");
+var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
+users = [];
+connections = [];
+server.listen(3000);
+app.get("/", function(req, resp) {
+    //route for localhost:3000
+    resp.sendFile(_dirname + "/index.html");
+})
+io.sockets.on("connection", function (socket) {
+    //when client connects to server
+    connections.push(sockets); //add plug details to custom array
+    console.log("connected : %s socket connected", connections.length); //curr connections
+    socket.on("disconnect", function (data) {
+        //client disc. frm server
+        connections.splice(connections.indexOf(socket), 1);
+        console.log("Disconnected : %s socket connected", connections.length); //curr connections
+    });
+    socket.on("send message", function (data) {
+        console.log(data);
+        io.seockets.emit("new message", { msg: data });
+    });
+});
+
+console.log('server is listening')
